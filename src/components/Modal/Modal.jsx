9a1fca2da3e961026451c440f23cf05ee55fe252
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-12-30 11:44:32 +0100

    2

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,34 @@
 import { useEffect } from 'react'
 import css from './Modal.module.css'
 
-export const Modal = (props) => {
+export const Modal = ({closeModal, modalData}) => {
 
   
   const handleCloseModal = (e) => {
     if (e.target === e.currentTarget) {
-      props.closeModal();
-    }
-  }
-
-  const handleModalOnEscClose = (e) => {
-    if (e.code === 'Escape') {
-      props.closeModal();
+      closeModal();
     }
   }
 
   useEffect(() => {
+
+    const handleModalOnEscClose = (e) => {
+    if (e.code === 'Escape') {
+      closeModal();
+    }
+  }
     document.documentElement.style.overflow = 'hidden';
     window.addEventListener('keydown', handleModalOnEscClose)
     return () => {
       document.documentElement.style.overflow = 'auto';
       window.removeEventListener('keydown', handleModalOnEscClose)
     }
-  }, [handleModalOnEscClose])
+  }, [closeModal])
   
   return (
     <div className={css.Overlay} onClick={handleCloseModal}>
       <div className={css.Modal}>
-        <img src={props.modalData} alt="" />
+        <img src={modalData} alt="" />
       </div>
     </div>
   )
